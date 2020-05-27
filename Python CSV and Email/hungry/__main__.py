3597b7c89099e2052ad39b3018e1ad8aec9eb830
Tzu-Hao Wang <asakura4@gmail.com>
2020-05-26 22:53:51 -0700

    add main.py

---
+++ Python CSV and Email/hungry/__main__.py
@@ -0,0 +1,22 @@
+
+import argparse
+#from data_manager import read_data
+from data_class import UserManager
+from utils.templates import get_template, render_context
+
+parser = argparse.ArgumentParser(prog="hungry")
+parser.add_argument("type", type=str, choices=['view', 'message'])
+#parser.add_argument("did_send", type=str, choices=['true','false'])
+parser.add_argument('-id','--user_id',type=int)
+parser.add_argument('-e','--email',type=str)
+
+args = parser.parse_args()
+
+# print(read_data(user_id=args.user_id))
+# print(read_data(email=args.email))
+
+if args.type =="view":
+	print(UserManager().get_user_data(user_id=args.user_id, email=args.email))
+elif args.type == "message":
+	print(UserManager().message_user(user_id=args.user_id, email=args.email))
+	print("send message") 
